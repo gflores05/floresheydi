1f73a8d4e666f891d3b52f0c5698d61d01011f44
gflores05 <gabriel.flores0503@gmail.com>
2020-08-01 23:33:20 -0600

    ✨ feat(footer): footer components

---
+++ src/components/Footer/Copyright/Copyright.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './Copyright.scss'
+
+const copyright = () => (
+    <ul class="copyright">
+        <li>&copy; Arreglos Florales Heydi. All rights reserved</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+    </ul>
+)
+
+export default copyright;
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
+import Contact from './Contact/Contact';
+import SocialNetworks from './SocialNetworks/SocialNetworks';
+import Copyright from './Copyright/Copyright';
+
 import './Footer.scss';
 
 const footer = () => (
-    <div>Footer</div>
+    <footer id="footer">
+        <div class="inner">
+            <Contact></Contact>
+            <SocialNetworks></SocialNetworks>
+            <Copyright></Copyright>
+        </div>
+    </footer>
 )
 
 export default footer;
