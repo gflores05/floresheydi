e45d507603757f629c76774f1bff513e87ed66bb
mac <mac@MacBook-Air-de-mac.local>
2020-08-25 23:29:38 -0600

    fix(config): validate redux dev tools

--- src/store/index.js
+++ src/store/index.js
@@ -23,9 +23,9 @@ export default function configureStore(initialState = { }) {
   ];
 
   const composeEnhancers =
-  process.env.NODE_ENV === "development"
+  process.env.NODE_ENV === "development" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    : null || compose;
+    : compose;
 
   const store = createStore(
     Reducers(history),
